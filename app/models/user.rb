9af407883b5d75ee8b91e5125335357fe3aa1004
Elad Meidar <elad@shinobidevs.com>
2022-05-13 20:48:18 +0300

    [INITIAL]

---
+++ app/models/user.rb
@@ -0,0 +1,41 @@
+class User
+  include Mongoid::Document
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :rememberable, :validatable, :omniauthable, omniauth_providers: ['twitter']
+
+  ## Database authenticatable
+  field :email,              type: String, default: ""
+
+  ## Rememberable
+  field :remember_created_at, type: Time
+
+  ## Twitter
+  field :twitter_handle, type: String
+  field :twitter_token, type: String
+  field :twitter_access_token, type: String
+  field :twitter_access_token_secret, type: String
+  field :twitter_uid, type: String
+  field :twitter_name, type: String
+  field :twitter_avatar_url, type: String
+
+  ## Trackable
+  # field :sign_in_count,      type: Integer, default: 0
+  # field :current_sign_in_at, type: Time
+  # field :last_sign_in_at,    type: Time
+  # field :current_sign_in_ip, type: String
+  # field :last_sign_in_ip,    type: String
+
+  ## Confirmable
+  # field :confirmation_token,   type: String
+  # field :confirmed_at,         type: Time
+  # field :confirmation_sent_at, type: Time
+  # field :unconfirmed_email,    type: String # Only if using reconfirmable
+
+  ## Lockable
+  # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
+  # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
+  # field :locked_at,       type: Time
+  include Mongoid::Timestamps
+end
